81bfdc38d30ba77d6d7567bd90a33c5523269b21
Kris Tung <ragdoll.washoku89@gmail.com>
2022-11-10 17:12:28 +0800

    vue.config.js 調整

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-cli-router-practice/' // vue-cli-router-practice 為repo名稱
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-cli-router-practice/' : '/',
+  // vue-cli-router-practice 為repo名稱
 };
