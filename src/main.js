e330ddee862ec9f2b5ddb432a617bda057cb18a6
Kris Tung <ragdoll.washoku89@gmail.com>
2022-12-06 20:47:12 +0800

    補上 router 套件說明

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router'; // router屬於模組化套件，並在 /router/index.js 中使用預設匯出，在Vue Cli中要使用套件的話只要「 import 某某套件名 」，並在底下加上 「use(某某套件名)」即可
 
 createApp(App).use(router).mount('#app');
 // 這裡.mount('#app')掛載的目標在/public/index.html裡面
